65f4b547d1d3232a43ab58e13a364f31da2b6d7d
amrodwaire <amrodwaire@gmail.com>
2026-04-09 15:17:25 +0300

    fix: database connection and new api url

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+﻿import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'com.yourname.busconnect',
+    appName: 'كوستر',
+    webDir: 'dist/public'
+};
+
+export default config;
