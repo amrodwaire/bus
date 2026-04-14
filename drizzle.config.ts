a2796933349300371d65ff7d161f3af4f7de8a79
amrodwaire <amrodwaire@gmail.com>
2026-04-14 22:44:48 +0300

    fix: apply production postgres database configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
@@ -5,6 +6,6 @@ export default defineConfig({
     out: "./drizzle",
     dialect: "postgresql",
     dbCredentials: {
-        url: "postgresql://bus_w3id_user:npZkYzgf50Xp6T68keN9oR6CDXYGw0lk@dpg-d79sr87kijhs73dui1eg-a/bus_w3id",
+        url: process.env.DATABASE_URL!,
     },
 });
